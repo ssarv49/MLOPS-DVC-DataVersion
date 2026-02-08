a08e0cdfd60a7c81302a5d24f10e91a134f5f95f
Shivam Sarvesh <shivampython49@gmail.com>
2026-02-08 10:30:20 +0530

    second version

--- mycode.py
+++ mycode.py
@@ -8,6 +8,9 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
 
 df = pd.DataFrame(data)
 
+new_row_loc = {'Name': 'GF1', 'Age': 28, 'City': 'San Francisco' }
+df.loc[len(df.index)] = new_row_loc
+
 data_dir = 'data'
 
 os.makedirs(data_dir, exist_ok=True)
